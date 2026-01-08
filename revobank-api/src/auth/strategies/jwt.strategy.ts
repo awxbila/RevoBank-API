082c344b3975c906451e852edb46e6ba9b00bf58
Nabilah Nur Hazimah <nablahnur54@gmail.com>
2026-01-08 13:46:54 +0700

    deploy M4

--- revobank-api/src/auth/strategies/jwt.strategy.ts
+++ revobank-api/src/auth/strategies/jwt.strategy.ts
@@ -5,7 +5,7 @@ import { ConfigService } from '@nestjs/config';
 import { PrismaService } from '../../prisma/prisma.service';
 
 @Injectable()
-export class JwtStrategy extends PassportStrategy(Strategy) {
+export class JwtStrategy extends PassportStrategy(Strategy, 'jwt') {
   constructor(
     private configService: ConfigService,
     private prisma: PrismaService,
